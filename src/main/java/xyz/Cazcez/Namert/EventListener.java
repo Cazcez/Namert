a4bdef27be0fa1a79980f376bf0a8a155af37be7
Cazcez <50046813+Cazcez@users.noreply.github.com>
2020-07-15 06:25:30 +0300

    Create EventListener.java

---
+++ src/main/java/xyz/Cazcez/Namert/EventListener.java
@@ -0,0 +1,18 @@
+package xyz.Cazcez.Namert;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class EventListener implements Listener 
+{
+
+    Main main;
+
+    public EventListener(Main main) 
+    {
+        this.main = main;
+    }
+    
+
+
+}
